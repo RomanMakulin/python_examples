2cb92ebaa5df0f56aca30039f6ab6a97919b4ea9
Roman <sup.makulin@mail.ru>
2022-12-17 18:02:48 +0300

    перемешанный список

--- sem2/example5.py
+++ sem2/example5.py
@@ -3,5 +3,9 @@ import random
 
 list = [3, 2, 1, 6]
 print(f'Исходный список: {list}')
-random.shuffle(list)
+
+for  i in range(len(list)-1, 0, -1):
+    a = random.randint(0, i + 1)
+    list[i], list[a] = list[a], list[i]
+    
 print(f'Перемешанный список: {list}')
