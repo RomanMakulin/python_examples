e91dc71a695ffbc65f682cb8928a56d1db893ba7
unknown <WayWayz>
2023-01-14 20:11:50 +0300

    Решил 4 задачу 5 семинара

---
+++ sem5/example4.py
@@ -0,0 +1,21 @@
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+with open('file_encode.txt', 'w') as data:
+    data.write('WWWWBWWBBBWB')
+
+with open('file_encode.txt', 'r') as data:
+    string = data.readline()
+data.close
+
+
+result = ''
+i = 0
+while i < len(string):
+    count = 1
+    while i + 1 < len(string) and string[i] == string[i + 1]:
+        count += 1
+        i += 1
+    result += str(count) + string[i]
+    i += 1
+
+print(result)  
