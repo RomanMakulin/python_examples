ac5dd6e09cc707a6ce3618d6ef79107443da265c
unknown <WayWayz>
2023-01-14 21:45:24 +0300

    Доработал 1 задачу 5 семинара

--- sem5/example1.py
+++ sem5/example1.py
@@ -1,5 +1,8 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+with open('sem5/file.txt', 'r') as data:
+    string = data.read()
+data.close
 
-string = 'Childrensabc are playing game. They are need to compare box an abc'
+
 new_string = [i for i in string.split() if 'abc' not in i]
 print(' '.join(new_string))
