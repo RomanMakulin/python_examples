efbd369cb3273ce250293aa8de586f83c7faa67d
unknown <WayWayz>
2023-01-14 21:59:56 +0300

    Научился создавать рандомный массив и перемешивать его

--- traning/traning_lambda.py
+++ traning/traning_lambda.py
@@ -1,16 +1,23 @@
 # Изучили функции map filter zip and another lambda
+
+import random
 # Создание массива
-array = [i for i in range(1, 11)]
+array = [i for i in range(1, 100)]
+random.shuffle(array)
+array = array[:10]
 print(array)
 
+# Фильтрация массива по четным/не четным
 array = list(filter(lambda i: not i%2, array))
 print(array)
 
+# Изменение элементов массива (+10 к каждому)
 array = list(map(lambda i:i + 10, array))
 print(array)
 
+# Добавление айди с помощью ZIP
 ids = ['a', 'b', 'c']
-
 array = list(zip(ids, array))
 
 print(array)
+
